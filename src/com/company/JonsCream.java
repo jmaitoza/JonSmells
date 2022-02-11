170aa77ffe1abebefcb9d3156b05b0fdfcaa4af4
Joshua Maitoza <60358495+jmaitoza@users.noreply.github.com>
2022-02-10 21:04:29 -0500

    Refactored tylers cream into JonsCream

---
+++ src/com/company/JonsCream.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class JonsCream
+{
+//    buckets of cream
+    private int buckets;
+
+
+    public static void main(String[] args)
+    {
+        JonsCream jonsCream = new JonsCream();
+        jonsCream.buckets = 10;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+        System.out.println("Jons Cream has " + jonsCream.buckets + " buckets of cream");
+        jonsCream.buckets = jonsCream.buckets - 1;
+    }
+
+}
--- src/com/company/TylersCream.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class TylersCream
-{
-    //buckets of cream
-    private int buckets;
-
-}
